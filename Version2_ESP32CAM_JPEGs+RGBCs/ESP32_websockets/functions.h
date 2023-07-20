79c95507ca87e920d63e0bd551dca256cea70a29
Teapack1 <ondrej.major@gmail.com>
2023-07-21 00:56:00 +0200

    camera settings

--- Version2_ESP32CAM_JPEGs+RGBCs/ESP32_websockets/functions.h
+++ Version2_ESP32CAM_JPEGs+RGBCs/ESP32_websockets/functions.h
@@ -56,16 +56,16 @@ void configESPCamera() {
   s->set_whitebal(s, 0);       // 0 = disable , 1 = enable
   s->set_awb_gain(s, 0);       // 0 = disable , 1 = enable
   s->set_wb_mode(s, 0);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
-  s->set_exposure_ctrl(s, 0);  // 0 = disable , 1 = enable
-  s->set_aec2(s, 1);           // 0 = disable , 1 = enable
-  s->set_ae_level(s, 0);       // -2 to 2
-  s->set_aec_value(s, 10);    // 0 to 1200
+  s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
+  s->set_aec2(s, 0);           // 0 = disable , 1 = enable
+  s->set_ae_level(s, 2);       // -2 to 2
+  s->set_aec_value(s, 800);    // 0 to 1200
   s->set_gain_ctrl(s, 0);      // 0 = disable , 1 = enable
   s->set_agc_gain(s, 0);       // 0 to 30
   s->set_gainceiling(s, (gainceiling_t)0);  // 0 to 6
   s->set_bpc(s, 0);            // 0 = disable , 1 = enable
   s->set_wpc(s, 0);            // 0 = disable , 1 = enable
-  s->set_raw_gma(s, 0);        // 0 = disable , 1 = enable
+  s->set_raw_gma(s, 1);        // 0 = disable , 1 = enable
   s->set_lenc(s, 0);           // 0 = disable , 1 = enable
   s->set_hmirror(s, 0);        // 0 = disable , 1 = enable
   s->set_vflip(s, 0);          // 0 = disable , 1 = enable
