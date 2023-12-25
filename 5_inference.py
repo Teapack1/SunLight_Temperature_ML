3865c7079f48d2b0b73fc3e6a7883b76a7de6e21
Teapack1 <ondrej.major@gmail.com>
2023-12-25 17:02:23 +0100

    inference with tflite

--- 5_inference.py
+++ 5_inference.py
@@ -5,66 +5,57 @@ import numpy as np
 import os
 import base64
 import joblib
-from keras.models import load_model
+import tflite_runtime.interpreter as tflite
 import time
 
-MODEL = os.join('MODEL', 'model_020.keras')
-SCALER = os.join('MODEL', 'dataset20_scaler.pkl')
+MODEL = os.path.join('MODEL', 'color_model.tflite')
+SCALER = os.path.join('MODEL', 'color_scaler.pkl')
 CAPTURE_INTERVAL = 2 #s
 
 scaler = joblib.load(SCALER)
-model = load_model(MODEL)
+interpreter = tflite.Interpreter(model_path=MODEL)
+interpreter.allocate_tensors()
+
+# Get input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 async def run_prediction(websocket):
+    await websocket.send(f"control_value:{0.0}")
     
-    image_received = False
-    light_specs_received = False
-    rgbw_values_received = False
+    while True:
+        rgbw_values_received = False
+        while not rgbw_values_received:
+            message = await websocket.recv()
+            message_type, data = message.split(':', 1)
 
-    while not (image_received and light_specs_received and rgbw_values_received):
-        message = await websocket.recv()
-        message_type, data = message.split(':', 1)
-
-        if message_type == 'image':
-            print("Received image")
-
-            decoded_data = base64.b64decode(data)
-            nparr = np.frombuffer(decoded_data, np.uint8)
-            img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-            cv2.imshow('Received Image', img_np)
-            cv2.waitKey(1)
-
-            image_received = True
-
-        elif message_type == "light_specs":
-            print("Received light specs")
-
-            try:
-                cct, lux = data.strip().split(',')
-                light_specs_received = True
-            except ValueError:
-                print("Invalid data received. Skipping capture.")
-                continue
-
-        elif message_type == "rgbw_values":
-            print("Received RGBW values")
-
-            rgbw_values = data.strip().split(',')
-            if len(rgbw_values) != 4:
-                print("Invalid data received. Skipping capture.")
-                continue
-
-            rgbw_scaled = scaler.transform(rgbw_values)
-            rgbw_values_received = True
-            
-    predicted_control_value = model.predict(rgbw_scaled)
-    await websocket.send(f"control_value:{predicted_control_value:.3f}")
-    print("Control value sent")
-
-    print(
-        f"Capturing frame with color values: {rgbw_values}, cct: {cct}, lux: {lux}, predicted control value: {predicted_control_value:.3f}")
-
-    time.sleep(CAPTURE_INTERVAL)    # Prediction pause
+            if message_type == "rgbw_values":
+                print("Received RGBW values")
+                rgbw_values = data.strip().split(',')
+                if len(rgbw_values) != 4:
+                    print("Invalid data received. Skipping capture.")
+                    continue
+                
+                try:
+                    rgbw_array = np.array(rgbw_values, dtype=float).reshape(1, -1)  # Convert to numpy array and reshape
+                    rgbw_scaled = scaler.transform(rgbw_array)
+                    rgbw_values_received = True
+                    print("here1")
+                    input_data = np.array(rgbw_scaled, dtype=np.float32)        
+                    interpreter.set_tensor(input_details[0]['index'], input_data)
+                    print("here2")
+                    interpreter.invoke()
+                    print("here3")
+                    predicted_control_value = interpreter.get_tensor(output_details[0]['index'])
+                    print("Control value sent")
+                    print(f"Capturing frame with color values: {rgbw_values}, predicted control value: {predicted_control_value}")
+        
+                    control_value = predicted_control_value.item()
+                    await websocket.send(f"control_value:{control_value:.3f}")
+                except Exception as e:
+                    print(f"Error processing the value: {e}")
+                
+                time.sleep(CAPTURE_INTERVAL)    # Prediction pause
     
 async def main():
     async with websockets.serve(run_prediction, "0.0.0.0", 8765):
